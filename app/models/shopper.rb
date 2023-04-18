30701fbcb988657341c237ae8e4268abf5fe2250
nehasadh-github <nehasadh008@gmail.com>
2023-04-17 20:17:25 -0400

    first commit

---
+++ app/models/shopper.rb
@@ -0,0 +1,9 @@
+class Shopper < ApplicationRecord
+  validates :userid, uniqueness: true, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+
+
+
+end
